5990153974884fee01a70724fcdbcda85d02ca6d
maze <782618517@qq.com>
2020-03-08 16:32:16 +0800

    文件读入

--- InputData.py
+++ InputData.py
@@ -1,2 +1,10 @@
 def input_data():
-    pass
+    data=[]
+    for i in range(30):
+        data.append(0)
+    f=open("data.txt","r")
+    for i in range(29):
+        line=f.readline().split()
+        data[int(line[0])]=int(line[1])
+    f.close()
+input_data()
