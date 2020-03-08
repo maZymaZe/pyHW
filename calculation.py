e1c08490cc30fcbe5dfd72339a6e27df033a5803
maze <782618517@qq.com>
2020-03-08 17:47:33 +0800

    计算

--- calculation.py
+++ calculation.py
@@ -1,2 +1,23 @@
-def cal():
-    pass
+from InputData import input_data
+def cal(data):
+    maxNum=data[1]
+    minNum=data[1]
+    minDay=1
+    maxDay=1
+    summ=0
+    for i in range(1,30):
+        summ+=data[i]
+        if data[i]>maxNum:
+            maxNum=data[i]
+            maxDay=i
+        if data[i]<minNum:
+            minDay=i
+            minNum=data[i]
+    tmp=data[1:30]
+    tmp=tmp.sort
+    ave=summ/29
+    mid=data[14]
+    return maxNum,maxDay,minNum,minDay,ave,mid
+data=input_data()
+cal(data)
+    
