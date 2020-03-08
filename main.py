131c281748a85b77f664342f93e6eb7fedec65cf
maze <782618517@qq.com>
2020-03-08 17:49:55 +0800

    main参数返回值更改

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from InputData import input_data
 from calculation import cal
 from gui import gui
 
-input_data()
-cal()
-gui()
+data=input_data()
+maxNum,maxDay,minNum,minDay,ave,mid=cal(data)
+gui(maxNum,maxDay,minNum,minDay,ave,mid,data)
